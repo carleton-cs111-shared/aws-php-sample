628e5563e903f683765dc72b3c960f740d9dd3c6
Dave Musicant <dmusicant@carleton.edu>
2017-06-21 16:17:28 -0500

    successfully sending Python program via student

--- sshtryit.php
+++ sshtryit.php
@@ -4,7 +4,7 @@ $path = getenv('HOME') . '/aws-php/';
 # IP address in local environmental variable AWSIP
 $connection = ssh2_connect(getenv('AWSIP'), 22);
 
-if (ssh2_auth_pubkey_file($connection, 'ec2-user', '~/.ssh/vmkeypair.pub',
+if (ssh2_auth_pubkey_file($connection, 'student', '~/.ssh/vmkeypair.pub',
                           '~/.ssh/vmkeypair.pem')) {
     echo "Authentication Successful!\n";
 } else {
@@ -12,7 +12,7 @@ if (ssh2_auth_pubkey_file($connection, 'ec2-user', '~/.ssh/vmkeypair.pub',
 }
 
 $pyfile = 'count.py';
-ssh2_scp_send($connection, "{$path}/{$pyfile}", "/home/ec2-user/{$pyfile}");
+ssh2_scp_send($connection, "{$path}/{$pyfile}", "/home/student/{$pyfile}");
 
 # Lots of great help here on handling streamed output, which I've used below:
 # https://secure.php.net/manual/en/function.ssh2-exec.php
